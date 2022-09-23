4c793dd079faa2aea370fe3365e3960cfcebdc76
pick <picknavin556@gmail.com>
2022-09-23 13:51:27 +0700

    first commit

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Supapon Pucknavin on 23/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
